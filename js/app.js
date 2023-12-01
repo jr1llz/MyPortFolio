1d86dc52e7db75c2eed5b6c180df1944e1052199
Fajril Yusin Supriadi <karinablood1@gmail.com>
2023-12-01 23:23:01 +0700

    Website Content Creator

---
+++ js/app.js
@@ -0,0 +1,141 @@
+const header = document.querySelector("header");
+
+const first_skill = document.querySelector(".skill:first-child");
+const sk_counters = document.querySelectorAll(".counter span");
+const progress_bars = document.querySelectorAll(".skills svg circle");
+
+const ml_section = document.querySelector(".milestones");
+const ml_counters = document.querySelectorAll(".number span");
+
+const links = document.querySelectorAll(".nav-link");
+
+const hamburger = document.querySelector(".hamburger");
+
+window.addEventListener("scroll", () => {
+  activeLink();
+  if (!skillsPlayed) skillsCounter();
+  if (!mlPlayed) mlCounter();
+});
+
+// Sticky Nav
+
+function stickyNavbar() {
+  header.classList.toggle("scrolled", window.pageYOffset > 0);
+}
+
+stickyNavbar();
+
+window.addEventListener("scroll", stickyNavbar);
+
+// Reveal Animation
+
+let sr = ScrollReveal({
+  duration: 2000,
+  distance: "60px",
+});
+
+sr.reveal(".showcase-info", { delay: 300 });
+sr.reveal(".showcase-image", { origin: "top", delay: 400 });
+
+// Skills Animation
+
+function hasReached(el) {
+  let topPosition = el.getBoundingClientRect().top;
+
+  if (window.innerHeight >= topPosition + el.offsetHeight) return true;
+  return false;
+}
+
+function updateCount(num, maxNum) {
+  let currentNum = +num.innerText;
+
+  if (currentNum < maxNum) {
+    num.innerText = currentNum + 1;
+    setTimeout(() => {
+      updateCount(num, maxNum);
+    }, 12);
+  }
+}
+
+let skillsPlayed = false;
+
+function skillsCounter() {
+  if (!hasReached(first_skill)) return;
+
+  skillsPlayed = true;
+
+  sk_counters.forEach((counter, i) => {
+    let target = counter.dataset.target;
+    let strokeValue = 427 - 427 * (target / 100);
+
+    progress_bars[i].style.setProperty("--target", strokeValue);
+
+    setTimeout(() => {
+      updateCount(counter, target);
+    }, 400);
+  });
+
+  progress_bars.forEach(
+    (p) => (p.style.animation = "progress 2s ease-in-out forwards")
+  );
+}
+
+// Services Counter Animation
+
+let mlPlayed = false;
+
+function mlCounter() {
+  if (!hasReached(ml_section)) return;
+  mlPlayed = true;
+  ml_counters.forEach((ctr) => {
+    let target = +ctr.dataset.target;
+
+    setTimeout(() => {
+      updateCount(ctr, target);
+    }, 400);
+  });
+}
+
+// Portfolio Filter Animation
+
+let mixer = mixitup(".portfolio-gallery", {
+  selectors: {
+    target: ".prt-card",
+  },
+  animation: {
+    duration: 500,
+  },
+});
+
+// Change Active Link On Scroll
+
+function activeLink() {
+  let sections = document.querySelectorAll("section[id]");
+  let passedSections = Array.from(sections)
+    .map((sct, i) => {
+      return {
+        y: sct.getBoundingClientRect().top - header.offsetHeight,
+        id: i,
+      };
+    })
+    .filter((sct) => sct.y <= 0);
+
+  let currSectionID = passedSections.at(-1).id;
+
+  links.forEach((l) => l.classList.remove("active"));
+  links[currSectionID].classList.add("active");
+}
+
+// Open & Close Hamburger Menu
+
+hamburger.addEventListener("click", () => {
+  document.body.classList.toggle("open");
+  document.body.classList.toggle("stopScrolling");
+});
+
+links.forEach((link) =>
+  link.addEventListener("click", () => {
+    document.body.classList.remove("open");
+    document.body.classList.remove("stopScrolling");
+  })
+);
